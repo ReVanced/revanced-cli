11d67bc1ead07ba6fe7671e442d38fed5202c042
oSumAtrIX <johan.melkonyan1@web.de>
2022-05-26 04:08:15 +0200

    chore: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.21")
-    implementation("app.revanced:revanced-patcher:+")
-    implementation("app.revanced:revanced-patches:+")
+    implementation("app.revanced:revanced-patcher:1.0.0-dev.16")
+    implementation("app.revanced:revanced-patches:1.0.0-dev.11")
 
     implementation("info.picocli:picocli:4.6.3")
 
