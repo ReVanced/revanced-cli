13d6405c472b61d3ea57e10348c97d6dc482794b
Itroublve <51215044+Itroublve@users.noreply.github.com>
2022-06-26 17:59:39 +0200

    refactor code

--- src/main/kotlin/app/revanced/utils/adb/Commands.kt
+++ src/main/kotlin/app/revanced/utils/adb/Commands.kt
@@ -3,6 +3,7 @@ package app.revanced.utils.adb
 import app.revanced.cli.command.MainCommand.logger
 import se.vidstige.jadb.JadbDevice
 import se.vidstige.jadb.RemoteFile
+import se.vidstige.jadb.ShellProcess
 import se.vidstige.jadb.ShellProcessBuilder
 import java.io.File
 
@@ -19,25 +20,27 @@ internal fun JadbDevice.buildCommand(command: String, su: Boolean = true): Shell
 
 internal fun JadbDevice.run(command: String, su: Boolean = true): Int {
     if (su) {
-        return this.buildCommand(command, true).start().waitFor()
+        return this.buildCommand(command).start().waitFor()
     }
+    
+    return CheckSU(command)!!.waitFor()
+}
 
-    // Avoid deadlock whilst checking for root access
+private fun JadbDevice.CheckSU(command: String): ShellProcess? {
     val byteArray = ByteArray(8) // Length of 'MagiskSU'
     val adbCommand = this.buildCommand(command, false).start()
+
+    // fix: deadlock with SuperSU
     val adbInputStream = adbCommand.inputStream
     adbInputStream.read(byteArray)
 
-    // Check if SuperSU or some other unsupported SU
     if (String(byteArray).filter { !it.isWhitespace() } == "SuperSU") {
         Constants.SUPERSU = true
+    } else if (String(byteArray).filter { !it.isWhitespace() }.contains("SU") && String(byteArray) != "MagiskSU") {
+        logger.warn("Unsupported SU, this process may fail...")
     }
-    else if (String(byteArray).filter { !it.isWhitespace() }.contains("SU") && String(byteArray) != "MagiskSU") {
-        logger.warn("Unsupported SU.")
-    }
-
     adbInputStream.close()
-    return adbCommand.waitFor()
+    return adbCommand
 }
 
 internal fun JadbDevice.copy(targetPath: String, file: File) {
