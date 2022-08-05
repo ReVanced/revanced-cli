edaf071e920d16d852e3a474405b0c6de59ad557
Itroublve <51215044+Itroublve@users.noreply.github.com>
2022-08-05 05:35:37 +0200

    refactor: update Enum name
    Co-authored-by: Sculas <contact@sculas.xyz>

--- src/main/kotlin/app/revanced/utils/adb/Enums.kt
+++ src/main/kotlin/app/revanced/utils/adb/Enums.kt
@@ -1,6 +1,6 @@
 package app.revanced.utils.adb
 
-enum class ROOT(val value: String) {
+enum class RootType(val value: String) {
     MAGISKSU("Magisk"),
     SUPERSU("SuperSU"),
     NONE_OR_UNSUPPORTED("none or unsupported")
