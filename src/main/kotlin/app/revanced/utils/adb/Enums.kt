9bdf0b54b7afa93cacad0718493f62f493c76895
Itroublve <51215044+Itroublve@users.noreply.github.com>
2022-08-05 05:34:26 +0200

    revert: 999224b23c811ed4a02d9b967e0f9b354c268db1

--- src/main/kotlin/app/revanced/utils/adb/Enums.kt
+++ src/main/kotlin/app/revanced/utils/adb/Enums.kt
@@ -1,6 +1,6 @@
 package app.revanced.utils.adb
 
-enum class ROOT(val name: String) {
+enum class ROOT(val value: String) {
     MAGISKSU("Magisk"),
     SUPERSU("SuperSU"),
     NONE_OR_UNSUPPORTED("none or unsupported")
