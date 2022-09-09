71c81510f7c081361fcc70b880dc09d96013d296
Sculas <contact@sculas.xyz>
2022-09-09 20:59:10 +0200

    build: update patcher dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 dependencies {
     implementation(kotlin("reflect"))
 
-    implementation("app.revanced:revanced-patcher:4.2.3")
+    implementation("app.revanced:revanced-patcher:4.4.0")
     implementation("info.picocli:picocli:4.6.3")
     implementation("com.android.tools.build:apksig:7.2.1")
     implementation("com.github.revanced:jadb:master-SNAPSHOT") // updated fork
