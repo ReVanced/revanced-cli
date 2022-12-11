836606b244f996c9649fbab7bdacf25f2cf4266a
oSumAtrIX <johan.melkonyan1@web.de>
2022-12-11 22:33:19 +0100

    fix: invalid header when writing a `ZipFile`

--- src/main/kotlin/app/revanced/utils/signing/align/ZipAligner.kt
+++ src/main/kotlin/app/revanced/utils/signing/align/ZipAligner.kt
@@ -1,28 +1,11 @@
 package app.revanced.utils.signing.align
 
-import app.revanced.utils.signing.align.zip.ZipFile
-import java.io.File
+import app.revanced.utils.signing.align.zip.structures.ZipEntry
 
 internal object ZipAligner {
     private const val DEFAULT_ALIGNMENT = 4
     private const val LIBRARY_ALIGNMENT = 4096
 
-    fun align(input: File, output: File) {
-        val inputZip = ZipFile(input)
-        val outputZip = ZipFile(output)
-
-        for (entry in inputZip.entries) {
-            val data = inputZip.getDataForEntry(entry)
-
-            if (entry.compression == 0.toUShort()) {
-                val alignment = if (entry.fileName.endsWith(".so")) LIBRARY_ALIGNMENT else DEFAULT_ALIGNMENT
-
-                outputZip.addEntryAligned(entry, data, alignment)
-            } else {
-                outputZip.addEntry(entry, data)
-            }
-        }
-
-        outputZip.finish()
-    }
+    fun getEntryAlignment(entry: ZipEntry): Int? =
+        if (entry.compression.toUInt() != 0u) null else if (entry.fileName.endsWith(".so")) LIBRARY_ALIGNMENT else DEFAULT_ALIGNMENT
 }
